758d823629fc4658a57cc1a0538796af62d788ab
M.Ameen <ameen55668@gmail.com>
2022-08-01 19:01:13 +0500

    complete the html & css of library projust by bootstrap

---
+++ js.js
@@ -0,0 +1,38 @@
+let book = document.getElementById('exampleFormControlInput1')
+let text = document.getElementById('exampleFormControlTextarea1')
+let a = document.getElementById('exampleRadios1')
+let b = document.getElementById('exampleRadios2')
+let c = document.getElementById('exampleRadios3')
+let save = document.getElementsByClassName('save')[0]
+let clear = document.getElementsByClassName('clear')[0]
+let table = document.getElementsByClassName('table')[0]
+// save.addEventListener('click',()=>{
+//     let type;
+//     if(a.checked === true){
+//         type = "a"
+//     }
+//     else if(b.checked === true){
+//         type = 'b'
+//     }
+//     else if(c.checked === true){
+//         type = 'c'
+//     }
+//     table.innerHTML += `<tr class="tr">
+//     <td>${book.value}</td>
+//     <td>${text.value}</td>
+//     <td>${type}</td>
+// </tr>`
+// book.value = ""
+// text.value = ""
+// })
+
+
+
+
+// over
+class values {
+    
+}
+save.addEventListener('click',()=> {
+
+})
