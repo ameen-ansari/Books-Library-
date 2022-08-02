4dc28ba78cf30aea416c9d504e2e4c4daa6f8600
M.Ameen <ameen55668@gmail.com>
2022-08-02 17:25:43 +0500

    93% complete hogayai ha

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 let book = document.getElementById('exampleFormControlInput1')
-let texta = document.getElementById('exampleFormControlTextarea1')
+let text = document.getElementById('exampleFormControlTextarea1')
 let a = document.getElementById('exampleRadios1')
 let b = document.getElementById('exampleRadios2')
 let c = document.getElementById('exampleRadios3')
@@ -7,20 +7,53 @@ let save = document.getElementsByClassName('save')[0]
 let clear = document.getElementsByClassName('clear')[0]
 let table = document.getElementsByClassName('table')[0]
 
-objm = function(name,text,val){
+// console.log(book,text,a,b,c,save,clear,table);
+
+objmk = function (name,fname,type) {
     this.name = name
-    this.text = text
-    this.val = val
+    this.fname = fname
+    this.type = type
 }
-save.addEventListener('click',()=>{
+var array = [1]
+objmk.prototype.show = function () {
+    table.innerHTML +=  `<tr class="tr">
+    <td >${array}</td>
+    <td class="w-25">${this.name}</td>
+    <td class="w-25">${this.fname}</td>
+    <td class="w-25">${this.type}</td>
+    <td class="w-25"><button type="button" onclick="(del(this))" class="btn btn-success rounded btn-sm">Delete</button></td>
+</tr>`
+}
+save.addEventListener('click',function() {
+    console.log('click event');
     let name = book.value
-    let text = texta.value
-    let val;
-    if(a.checked === true){val = 'a'}
-    else if(b.checked === true){val = 'b'}
-    else if(c.checked === true){val = 'c'}
-    var obj1 = new objm(name,text,val) 
+    let fname = text.value
+    if (a.checked === true) {var type = 'a' }
+    else if (b.checked === true) {var type = 'b' }
+    else if (c.checked === true) {var type = 'c' };
+    var obj1 = new objmk(name,fname,type)
     console.log(obj1);
-    book.value = ""    
-    text.value = ""    
+    obj1.show()
+    book.value = ""
+    text.value = ""
+    array.length = array++
 })
+clear.addEventListener('click',function () {
+    let conf = confirm('You are Sure to Delete All Library Books')
+    if(conf === true){
+    table.innerHTML = `<tr>
+    <th >No.</th>
+    <th class="w-25">Books</th>
+    <th class="w-25">Writer</th>
+    <th class="w-25">Type</th>
+    <th class="w-25">Delete button</th>
+</tr>`
+array = [1]
+}})
+del = (e)=>{
+    var r = e.parentElement.parentElement.remove()
+    console.log(r);
+    // array.length = array--
+   
+}
+
