384906678eb86977c42d0ff77960ec00259b29eb
M.Ameen <ameen55668@gmail.com>
2022-08-02 14:53:36 +0500

    's' create a function which makes objects

--- js.js
+++ js.js
@@ -1,38 +1,26 @@
 let book = document.getElementById('exampleFormControlInput1')
-let text = document.getElementById('exampleFormControlTextarea1')
+let texta = document.getElementById('exampleFormControlTextarea1')
 let a = document.getElementById('exampleRadios1')
 let b = document.getElementById('exampleRadios2')
 let c = document.getElementById('exampleRadios3')
 let save = document.getElementsByClassName('save')[0]
 let clear = document.getElementsByClassName('clear')[0]
 let table = document.getElementsByClassName('table')[0]
-// save.addEventListener('click',()=>{
-//     let type;
-//     if(a.checked === true){
-//         type = "a"
-//     }
-//     else if(b.checked === true){
-//         type = 'b'
-//     }
-//     else if(c.checked === true){
-//         type = 'c'
-//     }
-//     table.innerHTML += `<tr class="tr">
-//     <td>${book.value}</td>
-//     <td>${text.value}</td>
-//     <td>${type}</td>
-// </tr>`
-// book.value = ""
-// text.value = ""
-// })
 
-
-
-
-// over
-class values {
-    
+objm = function(name,text,val){
+    this.name = name
+    this.text = text
+    this.val = val
 }
-save.addEventListener('click',()=> {
-
+save.addEventListener('click',()=>{
+    let name = book.value
+    let text = texta.value
+    let val;
+    if(a.checked === true){val = 'a'}
+    else if(b.checked === true){val = 'b'}
+    else if(c.checked === true){val = 'c'}
+    var obj1 = new objm(name,text,val) 
+    console.log(obj1);
+    book.value = ""    
+    text.value = ""    
 })
